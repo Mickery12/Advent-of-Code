001861941d2aba60a373c5f48b2f8a7feea8cc21
Mickery12 <85701836+Mickery12@users.noreply.github.com>
2022-12-06 07:36:55 +0100

    Aoc 6 finished and refactored

--- AoC 6/src/PuzzleSolver.java
+++ AoC 6/src/PuzzleSolver.java
@@ -4,12 +4,41 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class PuzzleSolver {
-    
+
     private static List<String> createList(Path path) throws IOException {
-        return Files.readAllLines(Path.of(path.toUri()));
+        return Files.readAllLines(path);
     }
-    
-    public static String solvePuzzle(){
-        return "yeet";
+
+    private static int determineLength(int puzzlePart){
+        return puzzlePart == 1 ? 4 : 14;
+    }
+    private static boolean searchInArray(char[] chars){
+        for (int j = 0; j < chars.length; j++) {
+            for(int k = j+1; k < chars.length; k++){
+                if(chars[k] == chars[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int solvePuzzle(int puzzlePart) throws IOException {
+        var input = createList(Path.of("Input.txt"));
+
+        var sb = new StringBuilder(input.get(0));
+
+        char[] chars = new char[determineLength(puzzlePart)];
+
+        for (var i = 0; i < sb.length(); i++) {
+            chars[(i % chars.length)] = sb.charAt(i);
+            if(i > chars.length){
+                if(searchInArray(chars)){
+                    continue;
+                }
+                return i+1;
+            }
+        }
+        return 0;
     }
 }
