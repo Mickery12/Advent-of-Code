4da1a3cf75da362db7f3996790415e932bd41f58
Mike <idknowmw@gmail.com>
2022-12-08 09:27:32 +0100

    AoC 8 finished and not refactored

--- AoC 8/src/PuzzleSolver.java
+++ AoC 8/src/PuzzleSolver.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.List;
 
 public class PuzzleSolver {
@@ -10,72 +9,119 @@ public class PuzzleSolver {
         return Files.readAllLines(path);
     }
 
-    private static boolean checkIfVisible(int[][] visibleTrees, int i, int j){
+    private static boolean checkIfVisible(int[][] trees, int i, int j){
         boolean isVisible = true;
         for(int k = 0; k < i; k++){
-            if(visibleTrees[k][j] == 0){
+            if(trees[k][j] >= trees[i][j]){
                 isVisible = false;
                 break;
             }
         }
-        for(int k = visibleTrees.length-1; k > i; k--){
-            if(visibleTrees[k][j] == 0){
+        if(isVisible){
+            return true;
+        }
+        isVisible = true;
+        for(int k = trees.length-1; k > i; k--){
+            if(trees[k][j] >= trees[i][j]){
                 isVisible = false;
                 break;
             }
-            isVisible = true;
+            
         }
-
+        if(isVisible){
+            return true;
+        }
+        isVisible = true;
         for(int k = 0; k < j; k++){
-            if(visibleTrees[i][k] == 0){
+            if(trees[i][k] >= trees[i][j]){
                 isVisible = false;
                 break;
             }
-            isVisible = true;
         }
-
-        for(int k = visibleTrees[i].length-1; k > j; k--){
-            if(visibleTrees[i][k] == 0){
+        if(isVisible){
+            return true;
+        }
+        isVisible = true;
+        for(int k = trees[i].length-1; k > j; k--){
+            if(trees[i][k] >= trees[i][j]){
                 isVisible = false;
                 break;
             }
-            isVisible = true;
         }
         return isVisible;
+    }
+    
+    private static int countVisibleRange(int[][] trees, int i, int j){
+        int visibleRangeYP = 0;
+        int visibleRangeXP = 0;
+        int visibleRangeYN = 0;
+        int visibleRangeXN = 0;
+    
+        int offset = 1;
+        
+        if(i == 0 || j == 0 || i == trees.length-1 || j == trees[i].length-1){
+            return 0;
+        }
+        
+        for(int k = i-1; k >= 0; k--, offset++){
+            
+            if(trees[k][j] >= trees[i][j]){
+                visibleRangeYP = offset;
+                break;
+            }
+            else if(k==0){
+                visibleRangeYP = offset;
+            }
+        }
+        offset = 1;
+        for(int k = i+1; k < trees.length; k++, offset++){
+            if(trees[k][j] >= trees[i][j]){
+                visibleRangeYN = offset;
+                break;
+            }
+            else if(k==trees.length-1){
+                visibleRangeYN = offset;
+            }
+        }
+        offset = 1;
+        for(int k = j-1; k >= 0; k--, offset++){
+            if(trees[i][k] >= trees[i][j]){
+                visibleRangeXP = offset;
+                break;
+            }
+            else if(k==0){
+                visibleRangeXP = offset;
+            }
+        }
+    
+        offset = 1;
+        
+        for(int k = j+1; k < trees[i].length; k++, offset++){
+            if(trees[i][k] >= trees[i][j]){
+                visibleRangeXN = offset;
+                break;
+            }
+            else if(k==trees[i].length-1){
+                visibleRangeXN = offset;
+            }
+        }
+        return (visibleRangeXN*visibleRangeXP*visibleRangeYN*visibleRangeYP);
     }
 
     public static int solvePuzzle(int puzzlePart) throws IOException{
         List<String> input = createList(Path.of("Input.txt"));
         int[][] trees = new int[input.size()][input.get(0).length()];
-        int[][] locationVisibleTrees = new int[trees.length][trees[0].length];
         for (int j = 0; j < input.size(); j++) {
             for (int i = 0; i < input.get(0).length(); i++){
                 trees[j][i] = input.get(j).charAt(i)-48;
             }
         }
 
-        for (int i = 1; i < locationVisibleTrees.length-1; i++){
-            for (int j = 0; j < locationVisibleTrees[i].length; j+=98){
-                locationVisibleTrees[i][j] = 1;
-            }
-        }
-        for (int i = 0; i < locationVisibleTrees.length; i+= 98){
-            Arrays.fill(locationVisibleTrees[i], 1);
-        }
-        int visibleTrees = 99+97+99+97;
-        if(puzzlePart == 1){
-            for(int i = 1; i < 98; i++){
-                for (int j = 1; j < 98; j++){
-                    if((trees[i-1][j] < trees[i][j] || trees[i+1][j] < trees[i][j] || trees[i][j+1] < trees[i][j] || trees[i][j-1] < trees[i][j])
-                        && checkIfVisible(locationVisibleTrees, i, j)) {
-
-                        locationVisibleTrees[i][j] = 1;
-                    }
-                }
-            }
-            for (int[] i: locationVisibleTrees) {
-                for (int j: i) {
-                    if (j == 1){
+        if(puzzlePart == 1) {
+            int visibleTrees = 99+97+99+97;
+            for (int i = 1; i < trees.length-1; i++) {
+                for (int j = 1; j < trees[i].length-1; j++) {
+                    if(checkIfVisible(trees, i, j)){
                         visibleTrees++;
                     }
                 }
@@ -83,7 +129,13 @@ public class PuzzleSolver {
             return visibleTrees;
         }
         else {
-            return -1;
+            var visibleRange = 0;
+            for (int i = 0; i < trees.length; i++) {
+                for (int j = 0; j < trees[i].length; j++){
+                    visibleRange = Math.max(countVisibleRange(trees, i, j), visibleRange);
+                }
+            }
+            return visibleRange;
         }
     }
 }
